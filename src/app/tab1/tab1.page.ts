e1b3859f11fdf0e66615247f547b7c8651852b7e
hawa <sangarehawamadiougou@gmail.com>
2019-02-12 20:12:30 +0100

    v2.3

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,14 +3,28 @@ import { Component, NgModule
 import {
   ReactiveFormsModule, FormsModule, FormBuilder, NgForm
 } from '@angular/forms';
+//import {tab1Page} from "src/app/tab1";
+import {Subject} from 'rxjs';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+//@NgModule(
+ //   declarations:[
+ //       Tab1Page],
+//)
 export class Tab1Page {
- }
+  unAttribut : String = "Formulaire de contact";
+  formulaire={username:"",password:""} 
+onSubmitForm(form : NgForm)
+{  var nomuser=this.formulaire.username;
+  var motpass=this.formulaire.password;
+  
+} 
+}
 export class Membre {
   membre = {
     username: String,
@@ -21,8 +35,6 @@ export class Membre {
 
   };
 }
-//onSubmitForm(form : NgForm)
-//{
-//  console.log(form.value);
-//  this.dismissModal();
-//}
+//class EventEmitter <onSubmitForm> extends Subject{
+//@output() onSubmitForm = new EventEmitter();
+
